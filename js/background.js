00506fe3f9016d534d7a92a358aa9503cbad9dad
KyleAsaff <kyleasaff@gmail.com>
2014-07-16 16:37:39 -0300

    Added timer
    - Added timer
    - Fixed date parsing
    - Changes to manifest

--- js/background.js
+++ js/background.js
@@ -8,7 +8,7 @@ var localDataStore = {
     // Get value at specific index of array
     getIndexOf: function(key, index) {
         if (localStorage.getItem(key) === null)
-            return false;
+        return false;
         else
             return (JSON.parse(localStorage.getItem(key))[index]);
     },
@@ -65,50 +65,85 @@ function post(postID, threadTitle, threadTitleLink, threadReplies, threadViews, 
 
 var url = "http://forum.bodybuilding.com/";
 var repliesBuffer = [];
+var user = "nilekyle";
 
-// Search query
-$.get("http://forum.bodybuilding.com/search.php?do=process&query=60+posted+by+nilekyle&exactname=1&titleonly=0&searchdate=0&beforeafter=after&contenttypeid=1&sortby=dateline&order=descending&sortorder=descending&searchfromtype=vBForum%3APost&showposts=1&starteronly=0&searchthreadid=0&forumchoice[]=&childforums=1&replyless=0&type[]=1#top", function(data) {
+// create an offset value in local storage if none
+if (localStorage.getItem("offset") === null)
+    localStorage.setItem("offset", 0);
 
-    var $page = $(data);
+// store local storage value in offset
+var offset = localStorage.getItem("offset");
 
-    // Get the first 20 search results
-    var $block = $page.find(".blockbody").children().slice(0, 20);
+function fetchPosts() {
+    console.log(offset);
 
-    $.each($block, function(index, rawBuffer) {
-        var $rawBuffer = $(rawBuffer);
+    // Search query
+    var query = "http://forum.bodybuilding.com/search.php?do=process&query=" + offset + "+posted+by+" + user + "&exactname=1&titleonly=0&searchdate=0&beforeafter=after&contenttypeid=1&sortby=dateline&order=descending&sortorder=descending&searchfromtype=vBForum%3APost&showposts=1&starteronly=0&searchthreadid=0&forumchoice[]=&childforums=1&replyless=0&type[]=1#top"
+    $.get(query, function(data) {
 
-        // Put everything in the first result of search query in a buffer
-        var postDateBuffer = $rawBuffer.find("span.date").text().substr(0, 10);
-        var postTimeBuffer = $rawBuffer.find("span.date").text().substr(10);
-        var threadTitleBuffer = $rawBuffer.find("div.username_container h2 a:first").text();
-        var threadTitleLinkBuffer = url + $rawBuffer.find("div.username_container h2 a:first").attr("href");
-        var postAuthorLinkBuffer = url + $rawBuffer.find("div.username_container a:last").attr("href");
-        var postAuthorBuffer = $rawBuffer.find("div.username_container a:last").text();
-        var postIDBuffer = $rawBuffer.attr("id");
-        var postLinkBuffer = url + $rawBuffer.find("h3.posttitle a:first").attr("href");
-        var postDescBuffer = $rawBuffer.find("h3.posttitle a:first").text();
-        var threadRepliesBuffer = $rawBuffer.find("dl.userstats dd:first").text();
-        var threadViewsBuffer = $rawBuffer.find("dl.userstats dd:last").text();
+        var $page = $(data);
 
-        // Create a post object with the buffer data
-        var postBuffer = new post(postIDBuffer, threadTitleBuffer, threadTitleLinkBuffer, threadRepliesBuffer, threadViewsBuffer, postAuthorBuffer, postAuthorLinkBuffer, postDateBuffer, postTimeBuffer, postDescBuffer, postLinkBuffer);
+        // Get the first 20 search results
+        var $block = $page.find(".blockbody").children().slice(0, 20);
 
-        // fill new array of data to concat with data in localstore
-        if (postBuffer.postID === localDataStore.getIndexOf("replies", 0).postID) {
-            localDataStore.appendToFront("replies", repliesBuffer)
+        $.each($block, function(index, rawBuffer) {
+            var $rawBuffer = $(rawBuffer);
+
+            // Put everything in the first result of search query in a buffer
+            var postDateBuffer = $rawBuffer.find("span.date").clone().children().remove().end().text();
+            var dateLength = postDateBuffer.length;
+            postDateBuffer = postDateBuffer.substring(0, dateLength - 1);
+
+            var postTimeBuffer = $rawBuffer.find("span.time").text();
+            var threadTitleBuffer = $rawBuffer.find("div.username_container h2 a:first").text();
+            var threadTitleLinkBuffer = url + $rawBuffer.find("div.username_container h2 a:first").attr("href");
+            var postAuthorLinkBuffer = url + $rawBuffer.find("div.username_container a:last").attr("href");
+            var postAuthorBuffer = $rawBuffer.find("div.username_container a:last").text();
+            var postIDBuffer = $rawBuffer.attr("id");
+            var postLinkBuffer = url + $rawBuffer.find("h3.posttitle a:first").attr("href");
+            var postDescBuffer = $rawBuffer.find("h3.posttitle a:first").text();
+            var threadRepliesBuffer = $rawBuffer.find("dl.userstats dd:first").text();
+            var threadViewsBuffer = $rawBuffer.find("dl.userstats dd:last").text();
+
+            // Create a post object with the buffer data
+            var postBuffer = new post(postIDBuffer, threadTitleBuffer, threadTitleLinkBuffer, threadRepliesBuffer, threadViewsBuffer, postAuthorBuffer, postAuthorLinkBuffer, postDateBuffer, postTimeBuffer, postDescBuffer, postLinkBuffer);
+
+            // fail safe if cant get post from page
+            if (postBuffer.postID === "")
+                return false;
+
+            // fill new array of data to concat with data in localstore
+            if (postBuffer.postID === localDataStore.getIndexOf("replies", 0).postID) {
+                localDataStore.appendToFront("replies", repliesBuffer)
+                repliesBuffer = [];
+                return false;
+            } else
+                repliesBuffer.push(postBuffer);
+        });
+
+        // append replies buffer to local storage if there is
+        if (repliesBuffer !== null) {
+            localDataStore.appendToFront("replies", repliesBuffer);
             repliesBuffer = [];
-            return false;
-        } else
-            repliesBuffer.push(postBuffer);
+        }
+
+        $(".response").append($block).html();
     });
 
-    // append replies buffer to local storage
-    localDataStore.appendToFront("replies", repliesBuffer);
-    repliesBuffer = [];
+    // add 1 to the offset and store new offset
+    offset = parseInt(offset) + 1;
+    localStorage.setItem("offset", offset);
 
-    $(".response").append($block).html();
-
+    //Logs for debugging
+    console.log(query);
     console.log(localDataStore.get("replies"));
 
+    // Reset offset after it reaches 99
+    if (parseInt(offset) > 98) {
+        localStorage.setItem("offset", 0);
+        offset = 0;
+    }
+}
 
-});
+fetchPosts();
+setInterval(fetchPosts, 60 * 500);
