c38a7aa568c83ef47b8cec093fe257d2cfea0cfc
KyleAsaff <kyleasaff@gmail.com>
2014-07-17 15:42:31 -0300

    Separated background.js and data.js
    - took the localdatastore function and put it in its own js file
    (data.js) to be called by popup.html
    
    - deleted old legacy files

---
+++ js/data.js
@@ -0,0 +1,48 @@
+var localDataStore = {
+    // Get item
+    get: function(key) {
+        if (localStorage.getItem(key) === null)
+            return false;
+        return (JSON.parse(localStorage.getItem(key)));
+    },
+    // Get value at specific index of array
+    getIndexOf: function(key, index) {
+        if (localStorage.getItem(key) === null)
+            return false;
+        else
+            return (JSON.parse(localStorage.getItem(key))[index]);
+    },
+
+    // Create new Item 
+    set: function(key, value) {
+        localStorage.setItem(key, JSON.stringify(value))
+    },
+
+    // Adds new items to the front of an array in localStorage
+    appendToFront: function(key, data) {
+
+        var t = data.constructor,
+            tmp, tmp2 = [],
+            concat;
+
+        switch (t) {
+
+            case Array:
+                tmp = localStorage.getItem(key);
+                tmp = (tmp === null) ? [] : JSON.parse(tmp);
+                $.each(data, function(index, post) {
+                    tmp2.push(post);
+                });
+                concat = tmp2.concat(tmp);
+                localStorage.setItem(key, JSON.stringify(concat));
+                break;
+
+            case post:
+                tmp = localStorage.getItem(key);
+                tmp = (tmp === null) ? [] : JSON.parse(tmp);
+                tmp.push(data);
+                localStorage.setItem(key, JSON.stringify(tmp));
+                break;
+        }
+    }
+};
