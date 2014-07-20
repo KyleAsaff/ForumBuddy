c773d44dab69686f72af85fdb031ee8f6e5ed7a7
KyleAsaff <kyleasaff@gmail.com>
2014-07-20 17:18:50 -0300

    fade animation
    fade animations now work

--- js/popup.js
+++ js/popup.js
@@ -19,20 +19,57 @@ $(document).ready(function() {
 	console.log(data);
 	$('div.container').append(data);
 
+	// handles clicking the x button on the post
 	$(".close").on('click', function() {
+		$('#myonoffswitch').prop('disabled', true);
 		var postNode = this.parentNode.parentNode.parentNode;
 		var id = $(postNode).attr('id');
 		var tempArray = localDataStore.get("replies");
 		console.log(tempArray[id].visible);
 
 		// post fade on click
+		$('#myonoffswitch').prop('disabled', true);
 		if (tempArray[id].visible === true) {
-			$("#" + id).addClass("fade");
+			$("#" + id).fadeTo("slow", 0.4, function() {
+				$("#" + id).addClass("fade");
+				$('#myonoffswitch').prop('disabled', false);
+			});
 			tempArray[id].visible = false;
 		} else {
-			$("#" + id).removeClass("fade");
+			$("#" + id).fadeTo("slow", 1.0, function() {
+				$("#" + id).removeClass("fade");
+				$('#myonoffswitch').prop('disabled', false);
+			});
 			tempArray[id].visible = true;
 		}
+
+		// get switch state from local storage (on click)
+		if (localStorage.getItem("state") === "on") {
+			$(".fade").show();
+			//$('#myonoffswitch').prop('disabled', false);
+		} else {
+			$('#myonoffswitch').prop('checked', false);
+			$("#" + id).fadeTo("slow", 0.0, function() {
+				$(".fade").hide();
+			});
+			$('#myonoffswitch').prop('disabled', true);
+			$("#" + id).fadeTo("fast", 0.4, function() {
+				$('#myonoffswitch').prop('disabled', false);
+			});
+		}
+		// store new visible state
+		localDataStore.set("replies", tempArray);
+	});
+
+	// handles clicking on the link -> mark as read
+	$(".title").on('click', function() {
+		var postNode = this.parentNode.parentNode.parentNode;
+		var id = $(postNode).attr('id');
+		var tempArray = localDataStore.get("replies");
+		console.log(tempArray[id].visible);
+
+			$("#" + id).addClass("fade");
+			tempArray[id].visible = false;
 
 		// get switch state from local storage (on click)
 		if (localStorage.getItem("state") === "on") {
@@ -56,7 +93,7 @@ $(document).ready(function() {
 		$(".fade").hide();
 	}
 
-	// store switch state in local storage
+	// store switch state in local storage on click
 	$("#myonoffswitch").on('click', function() {
 		if ($('#myonoffswitch').prop('checked')) {
 			$(".fade").show();
